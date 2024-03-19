f9336c4095efc67f597a81add52c690a95f9eb55
Doğukan ÖZEL <dogukanozel692@gmail.com>
2024-03-19 23:57:54 +0300

    Start

--- SistemAnaliziVeTasarimi2/Anasayfa.Designer.cs
+++ SistemAnaliziVeTasarimi2/Anasayfa.Designer.cs
@@ -28,13 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BtnHastaGiris = new System.Windows.Forms.Button();
+            this.BtnDoktorGiris = new System.Windows.Forms.Button();
+            this.BtnSekreterGiris = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnHastaGiris
+            // 
+            this.BtnHastaGiris.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.BtnHastaGiris.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnHastaGiris.ForeColor = System.Drawing.SystemColors.Window;
+            this.BtnHastaGiris.Location = new System.Drawing.Point(78, 248);
+            this.BtnHastaGiris.Name = "BtnHastaGiris";
+            this.BtnHastaGiris.Size = new System.Drawing.Size(125, 50);
+            this.BtnHastaGiris.TabIndex = 0;
+            this.BtnHastaGiris.Text = "Hasta Girişi";
+            this.BtnHastaGiris.UseVisualStyleBackColor = false;
+            this.BtnHastaGiris.Click += new System.EventHandler(this.BtnHastaGiris_Click);
+            // 
+            // BtnDoktorGiris
+            // 
+            this.BtnDoktorGiris.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.BtnDoktorGiris.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnDoktorGiris.ForeColor = System.Drawing.SystemColors.Window;
+            this.BtnDoktorGiris.Location = new System.Drawing.Point(228, 248);
+            this.BtnDoktorGiris.Name = "BtnDoktorGiris";
+            this.BtnDoktorGiris.Size = new System.Drawing.Size(125, 50);
+            this.BtnDoktorGiris.TabIndex = 0;
+            this.BtnDoktorGiris.Text = "Doktor Girişi";
+            this.BtnDoktorGiris.UseVisualStyleBackColor = false;
+            this.BtnDoktorGiris.Click += new System.EventHandler(this.BtnDoktorGiris_Click);
+            // 
+            // BtnSekreterGiris
+            // 
+            this.BtnSekreterGiris.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.BtnSekreterGiris.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnSekreterGiris.ForeColor = System.Drawing.SystemColors.Window;
+            this.BtnSekreterGiris.Location = new System.Drawing.Point(378, 248);
+            this.BtnSekreterGiris.Name = "BtnSekreterGiris";
+            this.BtnSekreterGiris.Size = new System.Drawing.Size(125, 50);
+            this.BtnSekreterGiris.TabIndex = 0;
+            this.BtnSekreterGiris.Text = "Sekreter Girişi";
+            this.BtnSekreterGiris.UseVisualStyleBackColor = false;
+            this.BtnSekreterGiris.Click += new System.EventHandler(this.BtnSekreterGiris_Click);
+            // 
+            // Anasayfa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.BtnSekreterGiris);
+            this.Controls.Add(this.BtnDoktorGiris);
+            this.Controls.Add(this.BtnHastaGiris);
+            this.Name = "Anasayfa";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnHastaGiris;
+        private System.Windows.Forms.Button BtnDoktorGiris;
+        private System.Windows.Forms.Button BtnSekreterGiris;
     }
 }
 
--- SistemAnaliziVeTasarimi2/Anasayfa.cs
+++ SistemAnaliziVeTasarimi2/Anasayfa.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SistemAnaliziVeTasarimi2.Doktor;
+using SistemAnaliziVeTasarimi2.Hasta;
+using SistemAnaliziVeTasarimi2.Sekreter;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +18,27 @@ namespace SistemAnaliziVeTasarimi2
         public Anasayfa()
         {
             InitializeComponent();
+        }
+
+        private void BtnHastaGiris_Click(object sender, EventArgs e)
+        {
+           HastaGirisSayfasi hst = new HastaGirisSayfasi();
+            hst.Show();
+            this.Hide();
+        }
+
+        private void BtnDoktorGiris_Click(object sender, EventArgs e)
+        {
+            DoktorGirisSayfasi dr = new DoktorGirisSayfasi();
+            dr.Show();
+            this.Hide();
+        }
+
+        private void BtnSekreterGiris_Click(object sender, EventArgs e)
+        {
+            SekreterGirisSayfasi skrt = new SekreterGirisSayfasi();
+            skrt.Show();
+            this.Hide();
         }
     }
 }
